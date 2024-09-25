a0ff8eeb4203bf272869ff9eca4fff84e8c5f997
Christopher Bradley <christy.bradley@live.co.uk>
2024-09-25 01:49:00 +0100

    Updated main program.cs with calculator script

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Obtaining first interger input
+Console.WriteLine("Type in the first number followed by the enter key.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Obtaining second interger input
+Console.WriteLine("Type in the second number followed by the enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// creating the sum for two interger inputs
+int sumTotal = firstNumber + secondNumber;
+
+//Outputting the total sum & result
+Console.WriteLine(" The sum of {0}+{1}={2}" , firstNumber , secondNumber , sumTotal);
